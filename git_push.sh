570d9e9ba191347e11e1b4ccd2280dd7450fa06b
hongzh.zhang <hongzh.zhang@qunar.com>
2020-08-14 10:25:24 +0800

    ${comment}

--- git_push.sh
+++ git_push.sh
@@ -1,7 +1,7 @@
 # @Author: Marte
 # @Date:   2020-08-13 11:17:35
 # @Last Modified by:   Marte
-# @Last Modified time: 2020-08-13 11:19:53
+# @Last Modified time: 2020-08-14 10:25:03
 
 
 if [[ $# -eq 1 ]]; then
@@ -10,7 +10,10 @@ else
     comment="update notes"
 fi
 
+echo "拉取远程分支"
+git pull origin master
 
+echo "推送提交任务到远程"
 git add .
-git commit -m "${comment}"
+git commit -m '${comment}'
 git push origin master
