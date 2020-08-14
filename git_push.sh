b8288813a7f63aaa761572911c2d4df370dfa565
hongzh.zhang <hongzh.zhang@qunar.com>
2020-08-14 10:26:12 +0800

    ${comment}

--- git_push.sh
+++ git_push.sh
@@ -1,7 +1,7 @@
 # @Author: Marte
 # @Date:   2020-08-13 11:17:35
 # @Last Modified by:   Marte
-# @Last Modified time: 2020-08-14 10:25:03
+# @Last Modified time: 2020-08-14 10:26:01
 
 
 if [[ $# -eq 1 ]]; then
@@ -14,6 +14,7 @@ echo "拉取远程分支"
 git pull origin master
 
 echo "推送提交任务到远程"
+git status
 git add .
 git commit -m '${comment}'
 git push origin master
